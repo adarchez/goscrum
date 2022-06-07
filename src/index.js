69c9b631ae3126f10c492a345c13f77f82496c87
Amaro Darchez <amarodarchez@gmail.com>
2022-06-07 12:28:36 -0300

    update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from "./store/store"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="goscrum">
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
